f7491b4cfe4abbb8984e49a0ade6171afb1913d7
alunyapatrick2022 <patrickalunya106@gmail.com>
2024-08-16 17:43:18 +0300

    v1

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const port = 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontends')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Routes
-app.use('/', require('./routes/auth'));
-app.use('/dashboard', require('./routes/dashboard'));
+app.use('/auth', require('./netlify/functions/auth'));
+app.use('/dashboard', require('./netlify/functions/dashboard'));
 
 // Start server
 app.listen(port, () => {
